e9ec712fbbe47ed271280e53fff60f8138910389
tianyl <lty19b@gmail.com>
2022-05-27 16:57:57 +0800

    fix sync problems

--- region_server/toolkit/decompress.py
+++ region_server/toolkit/decompress.py
@@ -7,7 +7,7 @@ target_path:压缩后文件存放路径
 def decompress(src_path, target_path):
     # src_path=r"\chrome\chromedriver_win32.zip"
     # target_path="\chrome\数据"
-    if(not os.path.isdir(target_path)):
+    if(os.path.isdir(target_path)):
         z = zipfile.ZipFile(src_path, 'r')
         z.extractall(path=target_path)
         z.close()
